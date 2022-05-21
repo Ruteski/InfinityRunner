a7ff6fc0a971f07baf97ab77747e7bb8d967ee83
Ruteski <lruteski@gmail.com>
2022-05-21 15:51:11 -0300

    animacao do player andando e pulando, deteccao do chao para pular apenas 1x

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,10 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 2f;
     [SerializeField] private float _jumpForce = 8f;
+    [SerializeField] private Animator _anim;
+
     private Rigidbody2D _rb;
+    private bool _isJumping = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +20,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && !_isJumping) {
             _rb.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+            _anim.SetBool("Jumping", true);
+            _isJumping = true;
         }
     }
 
     private void FixedUpdate() {
         _rb.velocity = new Vector2(_speed, _rb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.layer == 6) {//ground
+            _isJumping = false;
+            _anim.SetBool("Jumping", false);
+        }
     }
 }
